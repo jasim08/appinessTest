81a2109aedf5389d654d561b211093d2d1a41523
jasim <jasimh@teezlelabs.com>
2019-12-11 08:16:56 +0530

    #project setup

--- server.js
+++ server.js
@@ -3,13 +3,17 @@ var express = require('express')
 var app = express();
 var path = require('path');
 var port = 3000;
+var mysql      = require('mysql');
 
-
-
+app.use(express.json());       // to support JSON-encoded bodies
+app.use(express.urlencoded({ extended: true })); // to support URL-encoded bodies
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
+
+
+
 
 
 app.get('/', function(req, res){
@@ -17,8 +21,26 @@ app.get('/', function(req, res){
 });
 
 app.get('/register', function(req, res){
-    res.render('register', { title: 'Express' });
-    res.send('/register redirected')
+    res.render('register');
+
+});
+
+app.post('/user/register', function(req, res){
+    console.log(req.body);
+    var connection = mysql.createConnection({
+        host     : 'localhost',
+        user     : 'root',
+        password : 'VpsPwd@08',
+        database : 'authentication'
+    });
+    connection.connect();
+    connection.query('SELECT * from userRole', function (error, results, fields) {
+        if (error) throw error;
+        console.log('The solution is: ', results[0].userRoleId);
+    });
+
+    connection.end();
+
 });
 
 app.listen(port, function(){
